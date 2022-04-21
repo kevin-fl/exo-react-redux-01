d292259f0a33464fd1d3707081970c818c9fdbbd
Aurélien <aurelien.strimelle@bstorm.be>
2022-04-21 09:41:47 +0200

    Initilize app layout

---
+++ src/containers/main-footer/main-footer.jsx
@@ -0,0 +1,7 @@
+const MainFooter = () => (
+    <footer>
+        <p>BXL Formation - React JS</p>
+    </footer>
+);
+
+export default MainFooter;
---
+++ src/containers/main-header/main-header.jsx
@@ -0,0 +1,7 @@
+const MainHeader = () => (
+    <header>
+        <h1>Demo 08 - React &amp; Redux</h1>
+    </header>
+);
+
+export default MainHeader;
